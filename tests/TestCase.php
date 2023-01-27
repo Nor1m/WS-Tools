15842f4b0c62ea2020875dc95284d2b4bf0b7893
v.mironov <v.mironov@whitewill.ru>
2023-01-27 22:46:51 +0400

    setUp

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void {
+        parent::setUp();
+        $_SERVER['DOCUMENT_ROOT'] = public_path();
+    }
 }
